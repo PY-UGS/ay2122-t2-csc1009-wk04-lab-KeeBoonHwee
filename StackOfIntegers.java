f6d8382995b7d0058201fd85784ec6f044d78af2
KeeBoonHwee <2717921k@student.gla.ac.uk>
2022-01-25 20:41:54 +0800

    Sticking to the UML design, removed private int variable this.index

--- StackOfIntegers.java
+++ StackOfIntegers.java
@@ -1,60 +1,61 @@
 public class StackOfIntegers {
     private int[] elements;
     private int size;
-    private int index;
 
     public StackOfIntegers()
     {
-        this.size = 16;
-        this.elements = new int[this.size];
-        this.index = -1;
+        this.size = -1;
+        this.elements = new int[16];  // default capacity of 16
     }
 
     public StackOfIntegers(int capacity)
     {
-        this.size = capacity;
-        this.elements = new int[this.size];
-        this.index = -1;
+        this.size = -1;
+        this.elements = new int[capacity];
     }
 
     public boolean empty()
     {
-        return (this.index > -1 ? false: true);
+        return (this.size > -1 ? false: true);
     }
 
-    public int peek()
+    public int peek() throws ArrayIndexOutOfBoundsException
     {
         if(!this.empty())
         {
-            return this.elements[this.index];
+            return this.elements[this.size];
         }
         else
         {
-            return -1;
+            throw new ArrayIndexOutOfBoundsException("Stack is empty, nothing to see here...");
         }
     }
 
-    public void push(int value)
+    public void push(int value) throws ArrayIndexOutOfBoundsException
     {
-        if(this.index + 1 < this.size)
+        if(this.size + 1 < this.elements.length)
         {
-            this.index += 1;
-            this.elements[this.index] = value;
+            this.size += 1;
+            this.elements[this.size] = value;
+        }
+        else
+        {
+            throw new ArrayIndexOutOfBoundsException("Stack has reached maximum capacity, you are unable to add further...");
         }
     }
 
-    public int pop()
+    public int pop() throws ArrayIndexOutOfBoundsException
     {
         if(!this.empty())
         {
-            int value = this.elements[this.index];
-            this.elements[this.index] = 0;
-            this.index -= 1;
+            int value = this.elements[this.size];
+            this.elements[this.size] = 0;
+            this.size -= 1;
             return value;
         }
         else
         {
-            return -1;
+            throw new ArrayIndexOutOfBoundsException("Stack is empty, nothing to pop!");
         }
     }
 
